d602687d18928137d92d4f065eebed749da396ab
Richard Barrett-Small <rickbs@gmail.com>
2013-07-27 16:43:02 +0100

    Grabbing user name and password.

---
+++ unstability.py
@@ -0,0 +1,8 @@
+import getpass
+import readability
+
+print "Welcome to unstability by @richbs"
+u = raw_input("What is your Readibility username? ")
+p = getpass.getpass("What is your Readability password? (No stored anywhere)")
+
+print "Thank you", u
