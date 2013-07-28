f26d8991ea2ae9b1c9c0d3796e1d18f5bc8a9054
Richard Barrett-Small <rickbs@gmail.com>
2013-07-28 18:38:07 +0100

    Working through the Instapaper CSV

--- unstability.py
+++ unstability.py
@@ -2,23 +2,38 @@ import codecs
 import csv
 import getpass
 import readability
+import os
 import sys
 
 
 class InstapaperCSV:
 
-    csv_file = None
+    csv_reader = None
+    links_by_category = {}
 
     def __init__(self, csv_filename):
 
-        self.csv_file = codecs.open(csv_filename, 'r', 'UTF-8')
-        print self.csv_file.read()
+        csv_file = open(csv_filename, 'r')
+        self.csv_reader = csv.reader(csv_file)
+        for l in self.csv_reader:
+            category = l[3]
+            link = l[0]
+            title = l[1]
+            # put links in tuple
+            if category in self.links_by_category:
+                self.links_by_category[category].append((link, title,))
+            else:
+                self.links_by_category[category] = [(link, title,)]
+
+        self.count_links()
+
+    def count_links(self):
+
+        for cat in self.links_by_category:
+
+            print 'Found', len(self.links_by_category[cat]), 'in', cat
 
 
-
-def count_links(csv_file):
-
-    pass
 if __name__ == "__main__":
 
     if len(sys.argv) > 1:
@@ -27,10 +42,18 @@ if __name__ == "__main__":
         exit("Please specify location of Instapaper CSV file.")
 
     print "Welcome to unstability by @richbs"
-    k = raw_input("What is your Readability API key?")
-    s = raw_input("What is your Readability API secret")
+    if 'API_KEY' in os.environ:
+        k = os.environ['API_KEY']
+    else:
+        k = raw_input("What is your Readability API key? ")
+
+    if 'API_SECRET' in os.environ:
+        s = os.environ['API_SECRET']
+    else:
+        s = raw_input("What is your Readability API secret? ")
+
     u = raw_input("What is your Readability account username? ")
-    p = getpass.getpass("What is your Readability password? (Not stored here)")
+    p = getpass.getpass("What is your Readability password? ")
 
     icsv = InstapaperCSV(instapaper_csv)
 
